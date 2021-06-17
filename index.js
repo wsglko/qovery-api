89dd466755e272b8e8497f34eb325d39ab246443
sks <wsglko1@gmail.com>
2021-06-17 13:43:16 +0530

    html page added

--- index.js
+++ index.js
@@ -9,12 +9,10 @@ const Image = require("./models/images")
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
+app.use(express.staticProvider(__dirname+'/public'))
 
 app.get('/', (req, res, next => {
-    res.json({
-      status:true,
-      message:"Welcome"
-    })
+    res.render('index.html')
 })
 
 app.get('/api/bstchecklist', async (req, res, next) => {
